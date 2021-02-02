569fed002a7f25ada755a1ad31aa1fc21f7520bf
WestleyCer <wacwestley@gmail.com>
2021-02-02 09:15:05 -0500

    removed comment

--- src/components/CampsiteInfoComponents.js
+++ src/components/CampsiteInfoComponents.js
@@ -28,14 +28,6 @@ class CampsiteInfo extends Component {
                                 -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                             </div>
                         );
-                    
-                    {/* OLD CODE to compare
-                        <div key={comment.id}>
-                            <p>{comment.text} <br/> --{comment.author},{" "}
-                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </p>
-                        </div> */}
-
                     })}
                 </div>
             );
