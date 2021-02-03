83bf1a2ddd9310ae9718a6feab09e5a9abd8892b
WestleyCer <wacwestley@gmail.com>
2021-02-03 05:46:34 -0500

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponents';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
@@ -20,13 +21,10 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={campsite => this.onCampsiteSelect(campsite)} />
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+                <Footer />
             </div>
         );
     }
