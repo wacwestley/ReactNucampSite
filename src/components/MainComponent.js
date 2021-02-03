57c218938e3db60e7a5080d4eac34660dfa6f8bb
WestleyCer <wacwestley@gmail.com>
2021-02-03 17:01:48 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -33,7 +33,16 @@ class Main extends Component {
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        };
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return(
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        }
 
         return (
             <div>
@@ -41,6 +50,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
