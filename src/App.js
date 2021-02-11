3fc94d0881646c920bebb3ff5bf198fa9f3ba2c9
WestleyCer <wacwestley@gmail.com>
2021-02-11 02:45:27 -0500

    Intro to Redux 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import './App.css';
 
-const store = ConfireStore();
+const store = ConfigureStore();
 
 class App extends Component {
     render() {
